5f0a5cfcb6a5d8e53f1bf64b159a2848ebbfdb1f
alancj731 <alanchenjian@gmail.com>
2026-03-30 21:38:31 -0500

    change prod url for cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://natual-query-frontend.vercel.app/"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://natual-query-frontend.vercel.app"],
     methods: ["POST"],
   })
 );
